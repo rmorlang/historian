9baf986f4173873ed0e66306d9ccdf098c0da63f
Rick Lee-Morlang <rick@lee-morlang.com>
2009-11-19 18:48:46 -0800

    configure rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,13 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'historian'
 require 'spec'
 require 'spec/autorun'
+begin
+  require 'rubygems'
+  require 'spackle'
+  Spackle.init :with => :spec_formatter
+rescue LoadError
+  puts "spackle gem not found -- continuing"
+end
 
 Spec::Runner.configure do |config|
-  
 end
