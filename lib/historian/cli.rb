87435750d601f657576f0781ca2525c5faf1fa15
Rick Lee-Morlang <rick@lee-morlang.com>
2010-12-19 21:39:15 -0800

    implemented commit_msg and post_commit commands

--- lib/historian/cli.rb
+++ lib/historian/cli.rb
@@ -3,19 +3,48 @@ require 'project_scout'
 #require 'thor/actions'
 
 class Historian::CLI < Thor
-  attr_accessor :git
+  attr_accessor :git, :history, :repo_directory
 
   default_task :help
 
   def initialize(*)
     super
-    repo_directory = ProjectScout.scan Dir.pwd, :for => :git_repository
-    self.git = Historian::Git.new(repo_directory, nil)
+    @repo_directory = ProjectScout.scan Dir.pwd, :for => :git_repository
+    @history = File.open File.join(repo_directory, "History.txt"), "w+"
+    history.sync = true
+    history.extend Historian::HistoryFile
+    self.git = Historian::Git.new(repo_directory, history)
   end
 
   desc "commit_msg FILE", "Git commit-msg hook for Historian. Not intended for manual use."
   def commit_msg(message_file)
+    return nil if File.exists? amend_flag_file
 
+    history_updated = false
+    release = nil
+    new_message = []
+    File.open(message_file).each_line do |line|
+      line = Historian::CommitMessage.parse_line line
+      new_message << line.to_s unless line.suppressed?
+      case line.significance
+      when :minor, :major, :patch
+        history.update_history line.significance => line.to_message_s
+        history_updated = true
+      when :release
+        release = line.to_message_s
+      end
+    end
+
+    if release
+      history_updated = true
+      history.update_history :release => release
+      File.open release_flag_file, "w"
+    end
+
+    if history_updated
+      File.open amend_flag_file, "w"
+      File.open(message_file,"w") { |f| f.puts new_message }
+    end
   end
 
   desc "install", "install Historian Git hooks into current repository."
@@ -26,7 +55,25 @@ class Historian::CLI < Thor
 
   desc "post_commit", "Git post-commit hook for Historian. Not intended for manual use."
   def post_commit
+    if File.exists? amend_flag_file
+      git.bundle_history_file
+      File.unlink amend_flag_file
+    end
 
+    if File.exists? release_flag_file
+      git.tag_release
+      File.unlink release_flag_file
+    end
+  end
+
+protected
+
+  def amend_flag_file
+    File.join(repo_directory, ".git", "historian-amend")
+  end
+
+  def release_flag_file
+    File.join(repo_directory, ".git", "historian-release")
   end
 end
 
--- spec/historian/cli_spec.rb
+++ spec/historian/cli_spec.rb
@@ -26,11 +26,112 @@ describe Historian::CLI do
   end
 
   describe "#commit-msg" do
-    context "when an amendment flag exists" do
-      it "does not parse the commit message"
+    before do
+      @message_file = "/tmp/historian_test_message"
+      @cli = Historian::CLI.new
     end
 
-    it "opens and parses the commit message"
+    context "when an amendment flag exists" do
+      before do
+        @amend_flag = File.join(repo_directory, ".git", "historian-amend")
+        File.open(@amend_flag, "w")
+      end
+
+      it "does not parse the commit message" do
+        File.should_not_receive(:open).with(@message_file)
+        @cli.commit_msg @message_file
+      end
+    end
+
+    context "when message contains no historian tokens" do
+      before do
+        File.open(@message_file, "w") do |f|
+          f.puts "test bugfix"
+        end
+        @cli.commit_msg @message_file
+      end
+
+      after do
+        File.unlink @message_file
+      end
+
+      it "doesn't create an amend flag" do
+        amend_flag = File.join(repo_directory, ".git", "historian-amend")
+        File.exists?(amend_flag).should be_false
+      end
+
+      it "doesn't update the history file" do
+        history_file = File.join repo_directory, "History.txt"
+        modified?(history_file).should be_false
+      end
+    end
+
+    context "when message contains historian tokens" do
+      before do
+        File.open(@message_file, "w") do |f|
+          f.puts "b:test bugfix"
+        end
+        @cli.commit_msg @message_file
+      end
+
+      after do
+        File.unlink @message_file
+      end
+
+      it "creates an amend flag" do
+        amend_flag = File.join(repo_directory, ".git", "historian-amend")
+        File.exists?(amend_flag).should be_true
+      end
+
+      it "updates the history file" do
+        history_file = File.join repo_directory, "History.txt"
+        history = File.readlines(history_file)
+        history.grep(/test bugfix/).should have(1).match
+        modified?(history_file).should be_true
+      end
+
+      it "strips the tokens out of the message file" do
+        File.read(@message_file).should_not match(/b:/)
+      end
+
+      it "updates the commit message with tokens and suppressed lines removed" do
+        File.read(@message_file).should eq("test bugfix\n")
+      end
+    end
+
+    context "when the message contains a release token" do
+      before do
+        File.open(@message_file, "w") do |f|
+          f.puts "!:Addled Adder"
+        end
+        @cli.commit_msg @message_file
+      end
+
+      after do
+        File.unlink @message_file
+      end
+
+      it "creates a release flag" do
+        release_flag = File.join(repo_directory, ".git", "historian-release")
+        File.exists?(release_flag).should be_true
+      end
+
+      it "creates an amend flag" do
+        amend_flag = File.join(repo_directory, ".git", "historian-amend")
+        File.exists?(amend_flag).should be_true
+      end
+
+      it "strips the tokens out of the message file" do
+        File.read(@message_file).should_not match(/!:/)
+      end
+
+      it "updates the history file" do
+        history_file = File.join repo_directory, "History.txt"
+        history = File.readlines(history_file)
+        history.grep(/== 0.0.1 Addled Adder/).should have(1).match
+        modified?(history_file).should be_true
+      end
+    end
   end
 
   describe "#install" do
@@ -53,28 +154,48 @@ describe Historian::CLI do
     end
   end
 
-  describe "parsing the commit message" do
-    context "when message contains historian tokens" do
-      it "creates a amendment flag"
-      it "updates the history file"
-      it "updates the commit message with tokens and suppressed lines removed"
-    end
-
-    context "when the message contains a release token" do
-      it "creates a release flag"
-    end
-  end
 
   describe "#post-commit" do
+    before do
+      @git = mock("Historian::Git",
+                  :bundle_history_file => nil,
+                  :tag_release => nil)
+      Historian::Git.stub(:new).and_return(@git)
+      @cli = Historian::CLI.new
+    end
+
     context "when an amendment flag exists" do
-      it "stages the history file"
-      it "commits the staged history file"
-      it "deletes the amendment flag"
+      before do
+        @amend_flag = File.join(repo_directory, ".git", "historian-amend")
+        File.open(@amend_flag, "w")
+      end
+
+      it "bundles the history file" do
+        @git.should_receive :bundle_history_file
+        @cli.post_commit
+      end
+
+      it "deletes the amendment flag" do
+        @cli.post_commit
+        File.exists?(@amend_flag).should_not be_true
+      end
     end
 
     context "when a release flag exists" do
-      it "tags the release"
-      it "deletes the release flag"
+      before do
+        @release_flag = File.join(repo_directory, ".git", "historian-release")
+        File.open(@release_flag, "w")
+      end
+
+      it "tags the release" do
+        @git.should_receive :tag_release
+        @cli.post_commit
+      end
+
+      it "deletes the release flag" do
+        @cli.post_commit
+        File.exists?(@release_flag).should_not be_true
+      end
     end
   end
 
